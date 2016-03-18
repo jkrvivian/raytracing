f10533a74825be128e09e5cbf0e54b4b6d37751b
jkrvivian <jkrvivian@gmail.com>
2016-03-18 23:09:25 +0800

    OpenMP

--- math-toolkit.h
+++ math-toolkit.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-static inline
+inline  __attribute__((always_inline)) 
 void normalize(double *v)
 {
     double d = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
@@ -16,45 +16,53 @@ void normalize(double *v)
     v[2] /= d;
 }
 
-static inline
+inline __attribute__((always_inline))
 double length(const double *v)
 {
     return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
 }
 
-static inline
+inline  __attribute__((always_inline)) 
 void add_vector(const double *a, const double *b, double *out)
-{
-    out[0] = a[0] + b[0];
+{ 
+	for(int i=0;i<3;i++)
+		out[i] = a[i] + b[i];
+    /*out[0] = a[0] + b[0];
     out[1] = a[1] + b[1];
-    out[2] = a[2] + b[2];
+    out[2] = a[2] + b[2];*/
 }
 
-static inline
+inline  __attribute__((always_inline)) 
 void subtract_vector(const double *a, const double *b, double *out)
-{
-    out[0] = a[0] - b[0];
+{ 
+	for(int i=0;i<3;i++)
+		out[i] = a[i] - b[i];
+    /*out[0] = a[0] - b[0];
     out[1] = a[1] - b[1];
-    out[2] = a[2] - b[2];
+    out[2] = a[2] - b[2];*/
 }
 
-static inline
+inline  __attribute__((always_inline)) 
 void multiply_vectors(const double *a, const double *b, double *out)
-{
-    out[0] = a[0] * b[0];
+{ 
+	for(int i=0;i<3;i++)
+		out[i] = a[i] * b[i];
+   /* out[0] = a[0] * b[0];
     out[1] = a[1] * b[1];
-    out[2] = a[2] * b[2];
+    out[2] = a[2] * b[2];*/
 }
 
-static inline
+inline __attribute__((always_inline)) 
 void multiply_vector(const double *a, double b, double *out)
-{
-    out[0] = a[0] * b;
+{ 
+	for(int i=0;i<3;i++)
+		out[i] = a[i] * b;
+    /*out[0] = a[0] * b;
     out[1] = a[1] * b;
-    out[2] = a[2] * b;
+    out[2] = a[2] * b;*/
 }
 
-static inline
+inline  __attribute__((always_inline)) 
 void cross_product(const double *v1, const double *v2, double *out)
 {
     out[0] = v1[1] * v2[2] - v1[2] * v2[1];
@@ -62,17 +70,19 @@ void cross_product(const double *v1, const double *v2, double *out)
     out[2] = v1[0] * v2[1] - v1[1] * v2[0];
 }
 
-static inline
-double dot_product(const double *v1, const double *v2)
+inline __attribute__((always_inline)) 
+double dot_product(const double *v1, const double *v2) 
 {
-    double dp = 0.0;
-    dp += v1[0] * v2[0];
+    double dp = 0.0; 
+	for(int i=0;i<3;i++)
+		dp += v1[i] * v2[i];
+    /*dp += v1[0] * v2[0];
     dp += v1[1] * v2[1];
-    dp += v1[2] * v2[2];
+    dp += v1[2] * v2[2];*/
     return dp;
 }
 
-static inline
+inline __attribute__((always_inline)) 
 void scalar_triple_product(const double *u, const double *v, const double *w,
                            double *out)
 {
@@ -80,7 +90,7 @@ void scalar_triple_product(const double *u, const double *v, const double *w,
     multiply_vectors(u, out, out);
 }
 
-static inline
+inline  __attribute__((always_inline)) 
 double scalar_triple(const double *u, const double *v, const double *w)
 {
     double tmp[3];
